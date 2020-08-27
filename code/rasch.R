d86c895fb33313dd77d73bcb7e7ebc3431a99c21
Felipe Fronchetti <fronchetti@usp.br>
2020-08-27 10:12:27 -0400

    Update: 27/8/20

--- code/rasch.R
+++ code/rasch.R
@@ -1,18 +1,14 @@
 library("eRm")
 source("convert_to_binary.R")
 
-trace(grDevices::png, quote({
-  if (missing(type) && missing(antialias)) {
-    type <- "cairo-png"
-    antialias <- "subpixel"
-  }
-}), print = FALSE)
+write.csv(responses_matrix, "../data/responses.csv")
 
+# Train the rasch model
 rm_1 <- RM(responses_matrix)
+# Get the beta values for each question
 betas <- -coef(rm_1)
 round(sort(betas), 2)
+# Plot the item characteristic curve
 plotjointICC(rm_1, item.subset = 1:10, cex = .6, lwd=1.5 )
-
-# abline(h = .5, col = "gray")
-# abline(v = 0, col = "black")
+# Plot the person-item map
 plotPImap(rm_1, cex.gen = .8, sorted = TRUE, irug = TRUE)
